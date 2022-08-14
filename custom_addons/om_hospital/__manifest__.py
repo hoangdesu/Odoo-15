9e599ff2e795a77ecb16be6fde6658d2d2685889
Hoang Nguyen <38454021+hoangdesu@users.noreply.github.com>
2022-08-14 20:09:49 +0700

    feat: create template manifest

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+
+{
+    'name': 'Sales',
+    'version': '1.2',
+    'category': '',
+    'summary': '',
+    'description': """
+    """,
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'assets': {},
+    'post_init_hook': '_synchronize_cron',
+    'license': 'LGPL-3',
+}
